1980167b6e4cc81ac0b6f8d5a092439a74264b5f
Eyal Liebermann אייל ליברמן <eyal.liebermann@gmail.com>
2014-03-27 14:53:58 +0200

    Privacy policy dialog upgraded to HTML WebView display Hebrew text supported through base64 encoding The English text is a google translation of the henrew one an should be updated

--- src/il/org/hasadna/opentrain/PrivacyPolicyActivity.java
+++ src/il/org/hasadna/opentrain/PrivacyPolicyActivity.java
@@ -1,5 +1,11 @@
 package il.org.hasadna.opentrain;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
@@ -13,5 +19,31 @@ public class PrivacyPolicyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.privacy_policy);
+        WebView webView = (WebView) findViewById(R.id.privacy_policy_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadData(readTextFromResource(R.raw.privacy_policy_base64),"text/html; charset=utf-8", "base64");
+        
     }
+    
+    private String readTextFromResource(int resourceID)
+    {
+		StringBuilder total = new StringBuilder();
+
+    	try{
+    		InputStream inputStream = getResources().openRawResource(resourceID);
+    		BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+    		String line;
+    		while ((line = r.readLine()) != null) {
+	           total.append(line);
+    		}
+    	}     
+       catch (IOException e){
+    	   e.printStackTrace();
+       }
+     
+       return total.toString();
+     
+    }
+
 }
+
