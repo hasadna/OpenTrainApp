f6ac8f502fc625b3c9ddf46cb594644e2553f2be
noam2030 <noam2030@gmail.com>
2014-05-12 17:20:54 +0300

    play store apk version with debug=false

---
+++ src/il/org/hasadna/opentrain/application/MainApplication.java
@@ -0,0 +1,18 @@
+package il.org.hasadna.opentrain.application;
+
+import android.app.Application;
+
+/**
+ * Created by Noam.m on 4/30/2014.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public void trackException(String description) {
+
+    }
+}
