8ba4e0aa842c56b218dc312b2fd0f7a0e1be04aa
Eyal Liebermann אייל ליברמן <eyal.liebermann@gmail.com>
2014-05-20 19:32:17 +0300

    Press on dump list item to open file All context menu items displayed in dialog theme

--- src/il/org/hasadna/opentrain/activity/ShareDumpFilesActivity.java
+++ src/il/org/hasadna/opentrain/activity/ShareDumpFilesActivity.java
@@ -16,9 +16,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -26,83 +28,127 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.MediaStore.Files;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShareDumpFilesActivity extends Activity {
-	
-	 private static final String LOGTAG= ShareDumpFilesActivity.class.getName();
-	private static final int SHARE_DUMP_REQUEST = 7;
-	 private File mLogsDir;
+
+	private static final String LOGTAG = ShareDumpFilesActivity.class.getName();
 	private File[] mFilesList;
 
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-		Log.d(LOGTAG,"OnCreate:");
-        super.onCreate(savedInstanceState);
-       // setContentView(R.layout.privacy_policy);
+	public class FilesListAdapter extends ArrayAdapter<File> {
 
-        setContentView(R.layout.share_dump_files); 
-        
-        mLogsDir= JsonDumper.getLogsDir(getApplicationContext());
-        
-        String logsPath=mLogsDir.getPath();
-		Log.d(LOGTAG,"OnCreate: logPath="+logsPath);
+		public FilesListAdapter(Context context, int textViewResourceId) {
+			super(context, textViewResourceId);
+		}
 
-        
-        TextView textView = (TextView) findViewById(R.id.dump_files_path);
-        String title = getResources().getString(R.string.dump_files_path);
-        String content=title+" "+logsPath;
-        textView.setText(content);    
-        
-        
-        ListView listView =(ListView) findViewById(R.id.dump_files_list);
-        
-        mFilesList=getFilesList();
-        
-        String[] values= new String[mFilesList.length];
-               
-        for(int i=0;i<values.length;i++){
-        	values[i]=mFilesList[i].getName();
-        }
+		public FilesListAdapter(Context context, int resource, List<File> items) {
+			super(context, resource, items);
+		}
 
-        
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,
-        	              android.R.layout.simple_list_item_1, android.R.id.text1, values);
-        
-        listView.setAdapter(adapter);
-    }
-	
-	 public void onClick_Share(View v) throws RemoteException {	 
-		 Log.i(LOGTAG,"onClick_Share:");
-		 Intent intent = PrepareEmailIntent();
-		 startActivity(intent);
-		 return;
-	 }
-	 	
-	private Intent PrepareEmailIntent() {
-		
-		Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
-		
-		 intent.setType("message/rfc822");
-		 intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"eyal.liebermann@gmail.com","noam2030@gmail.com"});
-		 intent.putExtra(Intent.EXTRA_CC,	new String[]{"opentrain-dev@googlegroups.com"});
-		 intent.putExtra(Intent.EXTRA_SUBJECT, PrepareMessageSubject());
-		 intent.putExtra(Intent.EXTRA_TEXT, PrepareMessageText());
-		 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
 
-		
-		ArrayList<Uri> uris = getUris();
-   	   	
-		 intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
-		return intent;
+			View v = convertView;
+			if (v == null) {
+				LayoutInflater vi;
+				vi = LayoutInflater.from(getContext());
+				v = vi.inflate(R.layout.file_list_item, null);
+			}
+
+			File file = getItem(position);
+			if (file != null) {
+				TextView tv = (TextView) v.findViewById(R.id.label);
+				tv.setBackgroundColor(position);
+				if (tv != null) {
+					tv.setText(file.getName());
+				}
+			}
+			return v;
+		}
 	}
 
-	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.d(LOGTAG, "OnCreate:");
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.privacy_policy);
+
+		setContentView(R.layout.share_dump_files);
+
+		File logsDir;
+
+		logsDir = JsonDumper.getLogsDir(getApplicationContext());
+		String logsPath = logsDir.getPath();
+		TextView textView = (TextView) findViewById(R.id.dump_files_path);
+		String title = getResources().getString(R.string.dump_files_path);
+		String content = title + " " + logsPath;
+		textView.setText(content);
+		
+		mFilesList = getFilesList(logsDir);
+		createListView();
+	}
+
+	private void createListView() {
+		ListView listView = (ListView) findViewById(R.id.dump_files_list);
+		FilesListAdapter adapter = new FilesListAdapter(
+				getApplicationContext(), R.layout.file_list_item,
+				Arrays.asList(mFilesList));
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {		
+		 @Override
+		 public void onItemClick(AdapterView<?> adapter, View v, int position,
+		 long arg3) {
+			 File file=(File) adapter.getItemAtPosition(position); 		
+			 Toast.makeText(getApplicationContext(), file.getAbsolutePath(),  Toast.LENGTH_SHORT).show();
+
+			 Intent intent=new Intent(Intent.ACTION_VIEW);
+			 intent.setDataAndType(Uri.fromFile(file),"text/plain");
+			 try{
+				 startActivity(intent);
+			 }
+			 catch(ActivityNotFoundException e){
+				 Log.w(LOGTAG,"createListView.listView.setOnItemClickListener.OnItemClickListener: No Activity found to handle Intent.");		
+				 Toast.makeText(getApplicationContext(),R.string.no_activity_found_to_view_file, Toast.LENGTH_LONG).show();
+			 }
+		 }
+		 });
+	}
+
+	public void onClick_Share(View v) throws RemoteException {
+		Log.i(LOGTAG, "onClick_Share:");
+		Intent intent = PrepareEmailIntent();
+		startActivity(intent);
+		return;
+	}
+
+	private Intent PrepareEmailIntent() {
+
+		Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+
+		intent.setType("message/rfc822");
+		intent.putExtra(Intent.EXTRA_EMAIL, new String[] {
+				"eyal.liebermann@gmail.com", "noam2030@gmail.com" });
+		intent.putExtra(Intent.EXTRA_CC,
+				new String[] { "opentrain-dev@googlegroups.com" });
+		intent.putExtra(Intent.EXTRA_SUBJECT, PrepareMessageSubject());
+		intent.putExtra(Intent.EXTRA_TEXT, PrepareMessageText());
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		ArrayList<Uri> uris = getUris();
+
+		intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+		return intent;
+	}
 
 	private String PrepareMessageSubject() {
 		Prefs prefs = Prefs.getInstance(getApplicationContext());
@@ -111,7 +157,7 @@ public class ShareDumpFilesActivity extends Activity {
 
 		sb.append(getResources().getString(R.string.dump_email_subject));
 		sb.append(" device_id=" + prefs.getDailyID());
-		
+
 		return sb.toString();
 	}
 
@@ -138,26 +184,25 @@ public class ShareDumpFilesActivity extends Activity {
 
 		return sb.toString();
 	}
-	
+
 	private ArrayList<Uri> getUris() {
 		ArrayList<Uri> uris = new ArrayList<Uri>();
-	    for (int i=0;i<20 && i<mFilesList.length;i++)
-	    {
-	    	if(mFilesList[i].exists()&& mFilesList[i].canRead()){
-		        Uri u = Uri.fromFile(mFilesList[i]);
-		        uris.add(u);
-	    	}
-	    }
+		for (int i = 0; i < 20 && i < mFilesList.length; i++) {
+			if (mFilesList[i].exists() && mFilesList[i].canRead()) {
+				Uri u = Uri.fromFile(mFilesList[i]);
+				uris.add(u);
+			}
+		}
 		return uris;
 	}
 
-	private File[] getFilesList() {
-		File[] logFiles = mLogsDir.listFiles();
-		
+	private File[] getFilesList(File logsDir) {
+		File[] logFiles = logsDir.listFiles();
+
 		Arrays.sort(logFiles, new Comparator<File>() {
 			public int compare(File f1, File f2) {
 				return (int) (0 - Long.valueOf(f1.getName().compareTo(
-								f2.getName())));//name is ordered by creation date
+						f2.getName())));// name is ordered by creation date
 			}
 		});
 		return logFiles;
