1c1bd51055135588f7c79f2472e08f62c1ef400a
android <noam2030@gmail.com>
2014-09-02 23:14:13 +0300

    texts update

--- src/il/org/hasadna/opentrain/client/activity/PrivacyPolicyActivity.java
+++ src/il/org/hasadna/opentrain/client/activity/PrivacyPolicyActivity.java
@@ -1,46 +1,45 @@
 package il.org.hasadna.opentrain.client.activity;
 
-import il.org.hasadna.opentrain.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.webkit.WebView;
+import il.org.hasadna.opentrain.R;
 
 public class PrivacyPolicyActivity extends Activity {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.privacy_policy);
-        WebView webView = (WebView) findViewById(R.id.privacy_policy_view);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadData(readTextFromResource(R.raw.privacy_policy_base64),"text/html; charset=utf-8", "base64");
-        
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String content=readTextFromResource(R.raw.privacy_policy);
+        textView.setText(Html.fromHtml(content));
+        Linkify.addLinks(textView, Linkify.ALL);
     }
-    
-    private String readTextFromResource(int resourceID)
-    {
-		StringBuilder total = new StringBuilder();
 
-    	try{
-    		InputStream inputStream = getResources().openRawResource(resourceID);
-    		BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
-    		String line;
-    		while ((line = r.readLine()) != null) {
-	           total.append(line);
-    		}
-    	}     
-       catch (IOException e){
-    	   e.printStackTrace();
-       }
-     
-       return total.toString();
-     
+    private String readTextFromResource(int resourceID) {
+        StringBuilder total = new StringBuilder();
+        try {
+            InputStream inputStream = getResources().openRawResource(resourceID);
+            BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = r.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return total.toString();
     }
 
 }
