75bcb8b50b71bd08fa97cd96b881fdf43701c462
elinabtl <ekleinerman@gmail.com>
2015-10-15 09:12:12 +0300

    Update GtfsStation.java

--- app/src/main/java/com/opentrain/app/model/GtfsStation.java
+++ app/src/main/java/com/opentrain/app/model/GtfsStation.java
@@ -4,14 +4,14 @@ package com.opentrain.app.model;
  * Created by Elina on 06/10/2015.
  */
 public class GtfsStation {
+    public String id;
+    public String name;
     public long enterUnixTimeMs;
     public Long exitUnixTimeMs;
-    public String name;
-    public String id;
 
-    public GtfsStation(String name, String id, long enterUnixTimeMs, long exitUnixTimeMs) {
-        this.name = name;
+    public GtfsStation(String id, String name, long enterUnixTimeMs, long exitUnixTimeMs) {
         this.id = id;
+        this.name = name;
         this.enterUnixTimeMs = enterUnixTimeMs;
         this.exitUnixTimeMs = exitUnixTimeMs;
     }
